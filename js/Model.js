f8f9bb22ef363d35f3f0cc8a964fac3d6b2185df
Sarah Lannigan <sarah.lannigan@gmail.com>
2015-07-07 21:21:45 -0400

    Initial commit, texture mapping and basic modelling APIs complete

---
+++ js/Model.js
@@ -0,0 +1,203 @@
+var body, armR, armL, legR, legL, face;
+var shoulderR, shoulderL, hipR, hipL;
+
+function CreateMeatBoy() {
+	var meat = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+
+	body = new BoxNode();
+	body.createObj(meat, 1.5, 1, 0.75);
+	// body.scale(0.5, 0.5, 0.5);
+
+	face = new TextureNode();
+	face.createObj(1, 1, '../images/meatboy.png', true, 1, 1);
+	body.addChild(face);
+	face.translate(0, 0, 0.377);
+
+	shoulderL = new JointNode();
+	body.addChild(shoulderL);
+	shoulderL.translate(0.75, 0, 0);
+	shoulderL.rotate(0,0,-0.5);
+
+	armL = new BoxNode();
+	armL.createObj(meat, 0.6, 0.3, 0.3);
+	shoulderL.addChild(armL);
+	armL.translate(0.1, -0.15, 0);
+
+	shoulderR = new JointNode();
+	body.addChild(shoulderR);
+	shoulderR.translate(-0.75, 0, 0);
+	shoulderR.rotate(0,0,0.5);
+
+	armR = new BoxNode();
+	armR.createObj(meat, 0.6, 0.3, 0.3);
+	shoulderR.addChild(armR);
+	armR.translate(-0.1, -0.15, 0);
+
+	hipL = new JointNode();
+	body.addChild(hipL);
+	hipL.translate(0.4, -0.5, 0);
+
+	legL = new BoxNode();
+	legL.createObj(meat, 0.5, 0.5, 0.5);
+	hipL.addChild(legL);
+	legL.translate(0, -0.05, 0);
+
+	hipR = new JointNode();
+	body.addChild(hipR);
+	hipR.translate(-0.4, -0.5, 0);
+
+	legR = new BoxNode();
+	legR.createObj(meat, 0.5, 0.3, 0.5);
+	hipR.addChild(legR);
+	legR.translate(0, -0.15, 0);
+
+	return body.obj;
+}
+
+function AnimateTest(time) {
+	// var factor = time*4;
+
+	// var armLRotation = new THREE.Euler();
+	// // var armRRotation = new THREE.Euler();
+	// armLRotation.x = 0.5*Math.sin(factor);
+	// armLRotation.y = 0.5*Math.sin(factor);
+	// // armRRotation.x = -0.5*Math.sin(factor);
+
+	// shoulderL.applyRotation(armLRotation);
+
+	// var legLRotation = new THREE.Euler();
+	// legLRotation.x = -0.5*Math.sin(factor);
+	// hipL.applyRotation(legLRotation);
+	// var legLTranslation = new THREE.Vector3(0, 0.05*Math.cos(factor) + 0.1, 0);
+	// hipL.applyTranslation(legLTranslation);
+	// hipL.applyTranslation(THREE.Vector3(0, 0.5*Math.cos(factor), 0));
+	// shoulderR.applyRotation(armRRotation);
+
+	// shoulderL.rotate(0.01, 0, 0);
+
+	// shoulderL.rotate(0.5*Math.sin(factor), 0, 0);
+
+	// shoulderR.rotation.x = -0.5*Math.sin(factor);
+	// shoulderR.rotation.y = 0.5*Math.sin(factor);
+	// armR.position.z = 0.1*Math.sin(factor);
+
+	// shoulderL.rotation.x = 0.5*Math.sin(factor);
+	// shoulderL.rotation.y = 0.5*Math.sin(factor);
+	// armL.position.z = -0.1*Math.sin(factor);
+}
+
+var floorCeilingGap = 5;
+var heightDiffH = 4
+var heightDiffL = -4;
+var unitSize = 3;
+var buildingMat = new THREE.MeshLambertMaterial({ color: 0xaaaaaa });
+
+function CreateBuildingBlock(length, isFlipped) {
+	var boxLength = length * unitSize;
+	var boxHeight = 50;
+	var boxDepth = unitSize;
+	var building = new BoxNode();
+	building.createObj(buildingMat, boxLength, boxHeight, boxDepth);
+
+	var buildingNode = new ModelNode();
+	buildingNode.addChild(building);
+
+	buildingNode.translate(boxLength/2, 0, 0);
+
+	if (isFlipped) {
+		building.translate(0, boxHeight/2, 0);
+		building.rotate(0, Math.PI, 0);
+	}
+	else {
+		building.translate(0, -boxHeight/2, 0);
+	}
+
+	return buildingNode;
+}
+
+function CreateBuilding(type, length, horizontalOffset) {
+	if (type == "s") type = "H";
+	if (type == "e") type = "l";
+
+	var building1 = CreateBuildingBlock(length, false);
+	var building2 = null;
+	var verticalOffset = 0;
+
+	if (type == "H" || type == "M" || type == "L") {
+		building2 = CreateBuildingBlock(length, true);
+		building2.translate(0, floorCeilingGap, 0);
+	}
+
+	if (type == "h" || type == "H") {
+		verticalOffset = heightDiffH;
+	}
+	else if (type == "l" || type == "L") {
+		verticalOffset = heightDiffL;
+	}
+
+	var group = new ModelNode();
+	group.addChild(building1);
+	if (!_.isNull(building2)) {
+		group.addChild(building2);
+	}
+
+	group.translate(horizontalOffset*unitSize, verticalOffset, 0);
+	// group.translate(horizontalOffset*unitSize, 0, 0);
+
+	return group;
+}
+
+var tempMat = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+function CreateChainsaw(type, prevType, horizontalOffset) {
+	var verticalOffset = 0;
+	if (type == "C") {
+		verticalOffset += floorCeilingGap;
+	}
+
+	if (prevType == "h" || prevType == "H") {
+		verticalOffset += heightDiffH;
+	}
+	else if (prevType == "l" || prevType == "L") {
+		verticalOffset += heightDiffL;
+	}
+
+	var chainsaw = new BoxNode();
+	chainsaw.createObj(tempMat, unitSize*0.9, unitSize*0.9, 0.1);
+
+	chainsaw.translate(horizontalOffset*unitSize + (unitSize/2), verticalOffset, 0);
+	return chainsaw;
+}
+
+function CreateLevel(map) {
+	if (_.isUndefined(map)) {
+		// map = "sss.hh.mmm.l.H.M.L.eee"
+		map = "s.hcmlHMCLe";
+	}
+	var mapNode = new ModelNode();
+
+	for (var i = 0, mapLocation = 0; i < map.length; i++, mapLocation++) {
+		var currChar = map.charAt(i);
+		var lengthOfSection = 1;
+		var buildingStartIndex = mapLocation;
+
+		if (currChar == "c" || currChar == "C") {
+			mapLocation--;
+			var prevChar = (i == 0) ? map.charAt(0) : map.charAt(i-1);
+			var chainsaw = CreateChainsaw(currChar, prevChar, mapLocation);
+			mapNode.addChild(chainsaw);
+			continue;
+		}
+
+		while (map.charAt(i+1) == currChar) {
+			i++;
+			lengthOfSection++;
+			mapLocation++;
+		}
+		if (currChar != ".") {
+			var building = CreateBuilding(currChar, lengthOfSection, buildingStartIndex);
+			mapNode.addChild(building);
+		}
+	}
+	mapNode.translate(-20,0,0);
+	return mapNode.obj;
+}
---
+++ js/ModelNode.js
@@ -0,0 +1,135 @@
+var ModelNode = function() {
+	this.parent = null;
+	this.children = [];
+	this.obj = new THREE.Object3D();
+
+	this.localMatrix = new THREE.Matrix4();
+	this.translation = new THREE.Vector3(0,0,0);
+	// this.quaternion = new THREE.Quaternion();
+	this.rotation = new THREE.Euler();
+	this.scaleVec = new THREE.Vector3(1,1,1);
+}
+
+_.extend(ModelNode.prototype, {
+	addChild: function(child) {
+		child.parent = this;
+		this.obj.add(child.obj);
+		this.children.push(child);
+		this.obj.updateMatrixWorld(true);
+	},
+
+	translate: function(x, y, z) {
+		this.translation.x += x;
+		this.translation.y += y;
+		this.translation.z += z;
+
+		this.obj.position.copy(this.translation);
+		this.obj.updateMatrixWorld(true);
+	},
+
+	rotate: function(x, y, z) {
+		this.rotation.x += x;
+		this.rotation.y += y;
+		this.rotation.z += z;
+
+		this.obj.rotation.copy(this.rotation);
+		this.obj.updateMatrixWorld(true);
+	},
+
+	scale: function(x, y, z) {
+		this.scaleVec.x = this.scaleVec.x*x;
+		this.scaleVec.y = this.scaleVec.y*y;
+		this.scaleVec.z = this.scaleVec.z*z;
+
+		this.obj.scale.copy(this.scaleVec);
+		this.obj.updateMatrixWorld(true);
+	},
+
+	applyTranslation: function(translation, updateGlobal) {
+		var t = new THREE.Vector3();
+		t.addVectors(translation, this.translation);
+
+		this.obj.position.copy(t);
+
+		if (_.isUndefined(updateGlobal) || updateGlobal == true) {
+			this.obj.updateMatrixWorld(true);
+		}
+	},
+
+	applyRotation: function(rotation, updateGlobal) {
+		rotation.x += this.rotation.x;
+		rotation.y += this.rotation.y;
+		rotation.z += this.rotation.z;
+
+		this.obj.rotation.copy(rotation);
+		
+		if (_.isUndefined(updateGlobal) || updateGlobal == true) {
+			this.obj.updateMatrixWorld(true);
+		}
+	},
+
+	applyScale: function(scale, updateGlobal) {
+		scale.x *= this.scaleVec.x;
+		scale.y *= this.scaleVec.y;
+		scale.z *= this.scaleVec.z;
+
+		this.obj.scale.copy(scale);
+		
+		if (_.isUndefined(updateGlobal) || updateGlobal == true) {
+			this.obj.updateMatrixWorld(true);
+		}
+	},
+
+	applyMatrix: function(matrix) {
+		var object = matrix.decompose();
+		var rotation = new THREE.Euler();
+		applyTranslation(object[0], false);
+		applyRotation(rotation.setFromQuaternion(object[1]), false);
+		applyScale(object[2], false);
+
+		this.obj.updateMatrixWorld(true);
+	}
+});
+
+var JointNode = function() {
+    // Inherit the constructor of ModelNode.
+    ModelNode.apply(this, arguments);
+}
+
+// Inherit all other methods of ModelNode.
+_.extend(JointNode.prototype, ModelNode.prototype, {
+
+});
+
+var BoxNode = function() {
+    // Inherit the constructor of ModelNode.
+    ModelNode.apply(this, arguments);
+
+    this.width;
+    this.height;
+    this.depth;
+}
+
+// Inherit all other methods of ModelNode.
+_.extend(BoxNode.prototype, ModelNode.prototype, {
+	createObj: function(material, width, height, depth) {
+		this.width = width;
+		this.height = height;
+		this.depth = depth;
+
+		var box = new THREE.BoxGeometry(width, height, depth);
+		this.obj = new THREE.Mesh(box, material);
+	}
+});
+
+var TextureNode = function() {
+	// Inherit the constructor of ModelNode.
+    ModelNode.apply(this, arguments);
+}
+
+// Inherit all other methods of ModelNode.
+_.extend(TextureNode.prototype, ModelNode.prototype, {
+	createObj: function(planeSizeX, planeSizeY, textureImage, tile, textureSizeX, textureSizeY) {
+		this.obj = TextureMapper(planeSizeX, planeSizeY, textureImage, tile, textureSizeX, textureSizeY)
+	}
+});
